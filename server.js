7e537d1bf67285f15152f650f6d9d3a9d740d890
Zohaib Tariq <se.zohaib@gmail.com>
2017-04-05 12:24:57 +0500

    server written with homepage URL

---
+++ server.js
@@ -0,0 +1,13 @@
+var port = 8080;
+var express = require('express');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+
+app.get('/', function (req,res) {
+	res.send("Hi dear!");
+});
+
+app.listen(port, function(){
+	console.log("server is runing on : http://127.0.0.1:"+port);
+});
